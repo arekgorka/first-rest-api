27f05d545eb76b0777e37c44163bfbd24388977a
Arkadiusz Górka <arekgorka91@gmail.com>
2022-08-05 21:05:27 +0200

    Add swagger vol2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,12 +1,14 @@
 package com.crud.tasks;
 
 import com.crud.tasks.domain.TaskDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TasksApplication extends SpringBootServletInitializer {
 //public class TasksApplication {
 
