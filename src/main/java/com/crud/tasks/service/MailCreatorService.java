453cacd5f7a5cf9fdeae27ee4c2b0626ec36079b
Arkadiusz Górka <arekgorka91@gmail.com>
2022-10-06 00:52:05 +0200

    uzupełnienie wartosci w wysylanym mailu v1

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,7 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,12 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -19,6 +27,14 @@ public class MailCreatorService {
         context.setVariable("message", message);
         context.setVariable("tasks_url","https://arekgorka.github.io/");
         context.setVariable("button", "Visit Website");
+        context.setVariable("admin_name",adminConfig.getAdminName());
+        context.setVariable("goodbay_message","See you soon!");
+        context.setVariable("preview_message","Add new card to trello");
+        context.setVariable("company_name",companyConfig.getCompanyName());
+        context.setVariable("company_mail",companyConfig.getCompanyMail());
+        context.setVariable("company_phone",companyConfig.getCompanyPhone());
+        context.setVariable("company_address",companyConfig.getCompanyAddress());
+        context.setVariable("company_address_number",companyConfig.getCompanyAddressNumber());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
